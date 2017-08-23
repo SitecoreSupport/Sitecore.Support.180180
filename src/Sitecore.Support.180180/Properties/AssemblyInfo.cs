cccd4d0125dd7469e4e12a1dafe3c8e6585d3f86
DK\dmsa <dmsa@sitecore.net>
2017-08-23 15:32:17 +0300

    Fix for issue #180180 --- [RTE]Field height of Real-Time HTML View is reduced after switching tabs

---
+++ src/Sitecore.Support.180180/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.180180")]
+[assembly: AssemblyProduct("Sitecore.Support.180180")]
+[assembly: ComVisible(false)]
